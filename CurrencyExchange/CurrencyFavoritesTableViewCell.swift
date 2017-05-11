e55436da06e66661dbe4221aeff8f858d7dc2c46
Daniel Henderson <tindrum@mac.com>
2017-05-11 15:54:14 -0700

    tab bar has multiple tabs

---
+++ CurrencyExchange/CurrencyFavoritesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CurrencyFavoritesTableViewCell.swift
+//  CurrencyExchange
+//
+//  Created by Daniel Henderson on 5/11/17.
+//  Copyright © 2017 Daniel Henderson. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyFavoritesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CurrencyExchange/WelcomeTabBarItem.swift
@@ -0,0 +1,13 @@
+//
+//  WelcomeTabBarItem.swift
+//  CurrencyExchange
+//
+//  Created by Daniel Henderson on 5/11/17.
+//  Copyright © 2017 Daniel Henderson. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeTabBarItem: UITabBarItem {
+
+}
